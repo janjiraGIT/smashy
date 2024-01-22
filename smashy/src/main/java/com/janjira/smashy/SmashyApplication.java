472d676a9f3d3fa740fc02db6cf480a90c0369d9
Janjira Eriksson <janjira.eriksson@gmail.com>
2024-01-22 12:41:28 +0100

    First commit

---
+++ smashy/src/main/java/com/janjira/smashy/SmashyApplication.java
@@ -0,0 +1,13 @@
+package com.janjira.smashy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmashyApplication.class, args);
+	}
+
+}
