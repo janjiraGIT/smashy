fdd5bc2e3dadf59b49d63f0ab37e5cebe14f57b6
Janjira Eriksson <janjira.eriksson@gmail.com>
2024-01-23 14:53:25 +0100

    Add Member entity

--- smashy/src/main/java/com/janjira/smashy/entity/Member.java
+++ smashy/src/main/java/com/janjira/smashy/entity/Member.java
@@ -1,2 +1,69 @@
-package com.janjira.smashy.entity;public class Member {
+package com.janjira.smashy.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private Long memberId;
+    private String memberName;
+    private String memberEmail;
+
+
+    public Member() {
+    }
+
+    public Member(Long id, Long memberId, String memberName, String memberEmail) {
+        this.id = id;
+        this.memberId = memberId;
+        this.memberName = memberName;
+        this.memberEmail = memberEmail;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", memberId=" + memberId +
+                ", memberName='" + memberName + '\'' +
+                ", memberEmail='" + memberEmail + '\'' +
+                '}';
+    }
+
 }
